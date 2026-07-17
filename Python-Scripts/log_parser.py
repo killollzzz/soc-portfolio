a64b29a1cd731818112b4c7dffd82fe829880fe8
nevezuha <danilkinvadim68@gmail.com>
2026-07-17 19:53:22 +0300

    Enhance log parser to include top 5 User-Agents
    Added functionality to track and display the top 5 User-Agents in log files.

--- Python-Scripts/log_parser.py
+++ Python-Scripts/log_parser.py
@@ -4,6 +4,7 @@ import sys
 filename = sys.argv[1] if len(sys.argv) > 1 else 'access.log'
 
 ips = []
+user_agents = []
 error_404 = 0
 error_500 = 0
 
@@ -13,12 +14,16 @@ with open(filename, 'r') as f:
         newstroka = line.split()
         ip = newstroka[0]
         ips.append(ip)
+        if len(newstroka) >= 10:
+            ua = newstroka[9]
+            user_agents.append(ua)
         if len(newstroka) >= 8 and newstroka[7] == '404':
             error_404 += 1
         if len(newstroka) >= 8 and newstroka[7] == '500':
             error_500 += 1
 
 popularip = Counter(ips).most_common(5)
+popularua = Counter(user_agents).most_common(5)
 
 print(f"Файл: {filename}")
 print("Топ-5 IP по запросам:")
@@ -27,3 +32,7 @@ for ip, count in popularip:
 
 print(f"\nОшибок 404: {error_404} из {len(lines)}")
 print(f"Ошибок 500: {error_500} из {len(lines)}")
+
+print("\nТоп-5 User-Agent'ов:")
+for ua, count in popularua:
+    print(f"{ua} — {count} раз")
